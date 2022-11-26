3d35d627a6effad953846cda2268c2f6dcb03d18
kennedybabu <kennedynjogu8@gmail.com>
2022-11-26 21:46:12 +0300

    feature: create the product-item component

---
+++ src/components/ProductItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProductItem = () => {
+  return (    
+    <div className="product mt-2 w-[220px] mx-auto shadow-md rounded-md h-[300px] bg-slate-400 flex flex-col items-center justify-center">
+        <p>card</p>
+    </div>
+  )
+}
+
+export default ProductItem
